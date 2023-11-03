a1572ce8fdd6543e2a7580119887ce6c62b9e681
lev <lleevv2020@gmail.com>
2023-11-03 19:49:44 +0400

    add

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import Logo from '../Logo'
+
 import classes from './App.module.scss'
 
 function App() {
-  return <section className={classes['app']}>Aviasales</section>
+  return (
+    <section className={classes['app']}>
+      <Logo />
+      <div className={classes['app__body']}>
+        <div className={classes['app__left-column']}>
+          <p>Filters</p>
+        </div>
+        <div className={classes['app__right-column']}>
+          <p>Sorting</p>
+          <p>Progress</p>
+          <p>Airlines</p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default App
